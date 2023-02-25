4164750a21d73467c52f5b120440a63fa8359ce9
Jason Paulos <jasonpaulos@users.noreply.github.com>
2023-02-24 18:49:21 -0800

    My thoughts about traces

--- ledger/simulation/trace.go
+++ ledger/simulation/trace.go
@@ -32,6 +32,9 @@ type TxnPath []uint64
 type TxnResult struct {
 	Txn              transactions.SignedTxnWithAD
 	MissingSignature bool
+
+	// Trace is the optional, highly detailed evaluation trace
+	Trace TransactionTrace
 }
 
 // TxnGroupResult contains the simulation result for a single transaction group
@@ -92,3 +95,51 @@ func makeSimulationResult(lastRound basics.Round, txgroups [][]transactions.Sign
 	}
 	return result
 }
+
+// OpcodeTrace contains the trace effects of a single opcode evaluation
+type OpcodeTrace struct {
+	// The PC of the opcode being evaluated
+	PC uint64
+
+	// TODO: additional effects, like stack and scratch space changes
+	// Ideally these effects contain only the "delta" information, unlike the
+	// current dryrun trace, which returns the _entire_ stack and scratch space
+	// for _every_ opcode.
+	//
+	// For stack, an interface that returns the first index that's changed and the rest of the stack
+	// seems like a good idea. E.g. if the stack changes from [A, B, C] to [A, X, Y], the delta effect
+	// could be something like { index: 1, elements: [X, Y] }. You can also think of it in terms of
+	// pushes and pops if you want, so { pops: 2, pushes: [X, Y] }; the only difference is that index
+	// is the absolute stack index while pops is a relative index from the end.
+	//
+	// * Note, this is not very efficient for opcodes like frame_bury and cover, which insert a single
+	//   value potentially very deep into the stack, so perhaps an additional special case could be
+	//   created to address that, maybe like { bury: X, depth: Y }? (I view this as nice to have but
+	//   not critical for our first implementation).
+	//
+	// For scratch space, the good news is we don't have any opcodes which mutate more than 1 slot
+	// at a time, and you can't ever delete a scratch value. So I think a more basic scheme is appropriate;
+	// something that includes the index being set and the value should be sufficient.
+	//
+	// A note about types: stack and scratch space is "untyped", meaning either uint64s or byteslices
+	// can be anywhere. Since JSON and msgpack arrays can also be untyped, I think it's a good idea
+	// to embrace this and encode either a number or a byte string for each value.
+}
+
+// TransactionTrace contains the trace effects of a single transaction evaluation (including its inners)
+type TransactionTrace struct {
+	// LogicSigTrace contains the trace for LogicSig evaluation, if a LogicSig signed this transaction
+	LogicSigTrace []OpcodeTrace
+	// AppTrace contains the trace for an app evaluation, if this is an app call transaction
+	AppTrace []OpcodeTrace
+	// InnerTraces contains the traces for inner transactions, if this transaction spawned any. This
+	// object only contains traces for inners that are immediate children of this transaction.
+	// Grandchild traces will be present inside the TransactionTrace of their parent.
+	InnerTraces []TransactionTrace
+}
+
+// Additional notes:
+// * OpcodeTrace.PC is not very useful without the actual program bytecode. _For now_, I think that's
+//   ok. In the future I think we will include the "initial state" of the simulation (e.g. every account,
+//   app, local state, asset, holding, and box that's touched), and that might the problem for us.
+//
